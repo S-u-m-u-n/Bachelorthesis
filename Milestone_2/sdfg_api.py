a6c4ad43f6a5301e50de05e5494d5d9e80a2c9d3
S-u-m-u-n <simonjacob@protonmail.com>
2021-07-28 12:30:10 +0200

    add explicit sdfg api version

--- Milestone_2/sdfg_api.py
+++ Milestone_2/sdfg_api.py
@@ -308,11 +308,11 @@ K_tile_map_entry, K_tile_map_exit = nested_state.add_map(
 warp_map_entry, warp_map_exit = nested_state.add_map(
         'Warp',
         dict(warp_i='thread_block_i*size_thread_block_tile_m:thread_block_i*size_thread_block_tile_m+size_thread_block_tile_m:size_warp_tile_m', warp_j='thread_block_j*size_thread_block_tile_n:thread_block_j*size_thread_block_tile_n+size_thread_block_tile_n:size_warp_tile_n'),
-        schedule=dace.dtypes.ScheduleType.Default)
+        schedule=dace.dtypes.ScheduleType.Default) # needs to be dace.dtypes.ScheduleType.GPU_Threadblock
 
 thread_tile_map_entry, thread_tile_map_exit = nested_state.add_map(
         'Thread_tile',
-        dict(thread_tile_i='warp_i*size_warp_tile_m:warp_i*size_warp_tile_m+size_warp_tile_m:size_thread_tile_m', thread_tile_j='warp_j*size_warp_tile_n:warp_j*size_warp_tile_n+size_warp_tile_n:size_thread_tile_n'),
+        dict(thread_tile_i='warp_i:warp_i+size_warp_tile_m:size_thread_tile_m', thread_tile_j='warp_j:warp_j+size_warp_tile_n:size_thread_tile_n'),
         schedule=dace.dtypes.ScheduleType.Default)
 
 # thread_K_map_entry, thread_K_map_exit = nested_state.add_map(
@@ -328,7 +328,7 @@ thread_tile_map_entry, thread_tile_map_exit = nested_state.add_map(
 
 map_entry, map_exit = nested_state.add_map(
         'matmul_map',
-        dict(i='thread_tile_i*size_thread_tile_m:thread_tile_i*size_thread_tile_m+size_thread_tile_m', j='thread_tile_j*size_thread_tile_n:thread_tile_j*size_thread_tile_n+size_thread_tile_n', k='k_tile:k_tile+size_thread_tile_k'),
+        dict(i='thread_tile_i:thread_tile_i+size_thread_tile_m', j='thread_tile_j:thread_tile_j+size_thread_tile_n', k='k_tile:k_tile+size_thread_tile_k'),
         schedule=dace.dtypes.ScheduleType.Default)
 
 nested_state.add_memlet_path(_A, thread_block_grid_map_entry, K_tile_map_entry, warp_map_entry, thread_tile_map_entry, map_entry, tasklet, dst_conn='__a', memlet=dace.Memlet(f"{_A.data}[i, k]"))
