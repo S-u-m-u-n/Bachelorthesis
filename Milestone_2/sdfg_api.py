15534ff01ace2bfe9c0861b4005ce89ca774d9d0
S-u-m-u-n <simonjacob@protonmail.com>
2021-07-30 12:29:29 +0200

    try swizzle_v3

--- Milestone_2/sdfg_api.py
+++ Milestone_2/sdfg_api.py
@@ -375,12 +375,12 @@ K_tile_map_entry, K_tile_map_exit = nested_state.add_map(
 warp_map_entry, warp_map_exit = nested_state.add_map(
         'Warp',
         dict(warp_i='0:size_thread_block_tile_m:size_warp_tile_m', warp_j='0:size_thread_block_tile_n:size_warp_tile_n'),
-        schedule=dace.dtypes.ScheduleType.GPU_ThreadBlock) # needs to be dace.dtypes.ScheduleType.GPU_ThreadBlock
+        schedule=dace.dtypes.ScheduleType.GPU_Device) # needs to be dace.dtypes.ScheduleType.GPU_ThreadBlock
 
 thread_tile_map_entry, thread_tile_map_exit = nested_state.add_map(
         'Thread_tile',
         dict(thread_tile_i='0:size_warp_tile_m:size_thread_tile_m', thread_tile_j='0:size_warp_tile_n:size_thread_tile_n'),
-        schedule=dace.dtypes.ScheduleType.GPU_Device)
+        schedule=dace.dtypes.ScheduleType.GPU_ThreadBlock)
 
 thread_K_map_entry, thread_K_map_exit = nested_state.add_map(
         'Thread_K',
@@ -470,7 +470,7 @@ nested_state.add_memlet_path(register_storage_C,
                         thread_block_grid_map_exit,
                         A_matmul_B_nested_state,
                         memlet=dace.Memlet.simple(A_matmul_B_nested_state.data,
-'thread_block_i*size_thread_block_tile_m + warp_i + thread_tile_i:thread_block_i*size_thread_block_tile_m + warp_i + thread_tile_i + size_thread_tile_m, thread_block_j*size_thread_block_tile_n + warp_j + thread_tile_j:thread_block_j*size_thread_block_tile_n + warp_j + thread_tile_j + size_thread_tile_n',
+'thread_block_i*size_thread_block_tile_m+warp_i + thread_tile_i:thread_block_i*size_thread_block_tile_m+warp_i + thread_tile_i + size_thread_tile_m, thread_block_j*size_thread_block_tile_n+warp_j + thread_tile_j:thread_block_j*size_thread_block_tile_n+warp_j + thread_tile_j + size_thread_tile_n',
 # '''thread_block_i*size_thread_block_tile_m+warp_i + size_thread_tile_m * (bitwise_and(right_shift(4 * (thread_tile_i / size_thread_tile_m) + (thread_tile_j / size_thread_tile_n), 1), 7))
 # :thread_block_i*size_thread_block_tile_m+warp_i + size_thread_tile_m * (bitwise_and(right_shift(4 * (thread_tile_i / size_thread_tile_m) + (thread_tile_j / size_thread_tile_n), 1), 7))
 # + size_thread_tile_m
