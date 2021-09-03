2ac99924202b78fd8191b9079a2476ecd2471866
Simon <jacobsi@gruyere0.cm.cluster>
2021-09-03 16:20:10 +0200

    add performance results

---
+++ Milestone_2/device_data_NVIDIA.py
@@ -0,0 +1,8 @@
+Name = "NVIDIA Tesla V100-PCIE-32GB"
+SMs = 80
+warps_per_SM = 2
+threads_per_warp = 32
+registers_per_thread_block = 65536
+registers_per_warp = 65536
+total_compute_cores = 5120
+capability_version = 7.0
