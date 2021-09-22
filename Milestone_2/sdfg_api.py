10f9a08a9167c22a81875b5e960da1b5fb7180e1
S-u-m-u-n <simonjacob@protonmail.com>
2021-09-22 17:16:52 +0200

    always set local memory to zero

--- Milestone_2/sdfg_api.py
+++ Milestone_2/sdfg_api.py
@@ -86,8 +86,8 @@ if args.verbose:
     helpers.print_info("Program launched with the following arguments: " + str(args), args.colorless)
 
 
-schedule = Schedule(load_k=8, thread_tile_m=4, thread_tile_n=4, thread_tile_k=4, warp_tile_m=32, warp_tile_n=16,
-                        thread_block_tile_m=32, thread_block_tile_n=16, thread_block_tile_k=640,
+schedule = Schedule(load_k=8, thread_tile_m=16, thread_tile_n=16, thread_tile_k=16, warp_tile_m=64, warp_tile_n=32,
+                        thread_block_tile_m=128, thread_block_tile_n=64, thread_block_tile_k=640,
                         SWIZZLE_thread_block=2, SWIZZLE_thread_tile=True, split_k=2, double_buffering=False)
 
 M = dace.symbol('M')
