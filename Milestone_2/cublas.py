ca512586f03ff51195b25f92eb3789e78e25db79
jacobsi <jacobsi@student.ethz.ch>
2021-11-12 17:29:44 +0100

    fix cublas

--- Milestone_2/cublas.py
+++ Milestone_2/cublas.py
@@ -29,12 +29,11 @@ else:
     dtype = dace.float64
     ndtype = np.float64
 
-M = np.int32(args.M)
-N=np.int32(args.N)
-K=np.int32(args.K)
+M_input = args.M
+N_input = args.N
+K_input = args.K
 alpha = ndtype(args.alpha)
 beta = ndtype(args.beta)
-
 
 @dace.program
 def matmul(A: dtype[M, K], B: dtype[K, N], C: dtype[M, N], alpha: dtype, beta: dtype):
@@ -42,7 +41,7 @@ def matmul(A: dtype[M, K], B: dtype[K, N], C: dtype[M, N], alpha: dtype, beta: d
 
 dace.libraries.blas.default_implementation = 'cuBLAS'
 for i in range(args.repetitions):
-    A = np.random.rand(M, K).astype(ndtype)
-    B = np.random.rand(K, N).astype(ndtype)
-    C = np.zeros((M, N)).astype(ndtype)
+    A = np.random.rand(M_input, K_input).astype(ndtype)
+    B = np.random.rand(K_input, N_input).astype(ndtype)
+    C = np.zeros((M_input, N_input)).astype(ndtype)
     matmul(A=A, B=B, C=C, alpha=alpha, beta=beta)
