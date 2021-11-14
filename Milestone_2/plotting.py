11b790454cf7c48b4fc7b6aab4294648bddb9c23
jacobsi <jacobsi@student.ethz.ch>
2021-11-14 02:39:26 +0100

    add missing optimization in test 1 and 2 -.-

--- Milestone_2/plotting.py
+++ Milestone_2/plotting.py
@@ -24,7 +24,7 @@ def read_nvprof_data(path_to_csv):
     # if (df[1])
     df = df.filter(['Duration', 'Name']).iloc[1:]
 
-    df = df[df['Name'].str.contains("Thread_block_grid|dgemm")].filter(['Duration']).reset_index(drop=True).apply(pd.to_numeric, errors='coerce').iloc[1:, :]
+    df = df[df['Name'].str.contains("Thread_block_grid|dgemm|sgemm")].filter(['Duration']).reset_index(drop=True).apply(pd.to_numeric, errors='coerce').iloc[1:, :]
     if flag:
         df /= 1000
     return df
@@ -46,7 +46,7 @@ if args.test == 1:
     combined_df.columns = ["u", "st", "stb", "st+stb"]
     fig = plt.figure()
     sns.violinplot(data=combined_df).set(xticklabels=["Unoptimized", "ST", "STB", "ST+STB"], ylabel="Runtime [ms]", title="M = 1024, N = 1024, K = 1024") # , xlabel=""
-    fig.savefig(path + "1024_1024_1024_comparison.png")
+    fig.savefig(path + "comparison.png")
     ### With Double Buffering
     db_df = read_nvprof_data(path + "double_buffering.csv")
     # db_v_df = read_nvprof_data(path + "double_buffering_vectorization.csv")
@@ -59,11 +59,11 @@ if args.test == 1:
     # db_v_st_stb_df = read_nvprof_data(path + "double_buffering_vectorization_swizzled_threads_swizzled_thread_blocks.csv")
     cutlass_df = read_nvprof_data(base_path + "cutlass.csv")
     cublas_df = read_nvprof_data(base_path + "cublas.csv")
-    combined_df_db = pd.concat([db_df, db_st_df, db_stb_df, db_st_stb_df, cutlass, cublas_df], axis=1)
-    combined_df_db.columns = ["db", "db+st", "db+stb", "db+st+stb", "cublas"]
+    combined_df_db = pd.concat([db_df, db_st_df, db_stb_df, db_st_stb_df, cutlass_df, cublas_df], axis=1)
+    combined_df_db.columns = ["db", "db+st", "db+stb", "db+st+stb", "cutlass", "cublas"]
     fig_db = plt.figure()
-    sns.violinplot(data=combined_df_db).set(xticklabels=["DB", "DB+ST", "DB+STB", "DB+ST+STB", "cuBLAS"], ylabel="Runtime [ms]", title="M = 1024, N = 1024, K = 1024 with double buffering") # , xlabel=""
-    fig_db.savefig(path + "1024_1024_1024_comparison_db.png")
+    sns.violinplot(data=combined_df_db).set(xticklabels=["DB", "DB+ST", "DB+STB", "DB+ST+STB", "CUTLASS", "cuBLAS"], ylabel="Runtime [ms]", title="M = 1024, N = 1024, K = 1024 with double buffering") # , xlabel=""
+    fig_db.savefig(path + "comparison_db.png")
 
 ### (4096 x 4096) x (4096 x 4096)
 if args.test == 2:
@@ -71,15 +71,18 @@ if args.test == 2:
     path = base_path + str(args.path) +'/'
     ### Without Double Buffering
     unoptimized_df = read_nvprof_data(path + "unoptimized.csv")
-    v_df = read_nvprof_data(path + "vectorization.csv")
+    # v_df = read_nvprof_data(path + "vectorization.csv")
     st_df = read_nvprof_data(path + "swizzled_threads.csv")
     stb_df = read_nvprof_data(path + "swizzled_thread_blocks.csv")
-    v_st_df = read_nvprof_data(path + "vectorization_swizzled_threads.csv")
-    combined_df = pd.concat([unoptimized_df, v_df, st_df, v_st_df], axis=1)
-    combined_df.columns = ["u", "v", "st", "stb", "v_st"]
+    st_stb_df = read_nvprof_data(path + "swizzled_threads_swizzled_thread_blocks.csv")
+    # v_st_df = read_nvprof_data(path + "vectorization_swizzled_threads.csv")
+    # v_stb_df = read_nvprof_data(path + "vectorization_swizzled_thread_blocks.csv")
+    # v_st_stb_df = read_nvprof_data(path + "vectorization_swizzled_threads_swizzled_thread_blocks.csv")
+    combined_df = pd.concat([unoptimized_df, st_df, stb_df, st_stb_df], axis=1)
+    combined_df.columns = ["u", "st", "stb", "st+stb"]
     fig = plt.figure()
-    sns.violinplot(data=combined_df).set(xticklabels=["Unoptimized", "Vectorization", "Swizzled_Threads", "Vectorization+Swizzled_Threads"], ylabel="Runtime [ms]", title="M = 4096, N = 4096, K = 4096") # , xlabel=""
-    fig.savefig(path + "4096_4096_comparison.png")
+    sns.violinplot(data=combined_df).set(xticklabels=["Unoptimized", "ST", "STB", "ST+STB"], ylabel="Runtime [ms]", title="M = 4096, N = 4096, K = 4096") # , xlabel=""
+    fig.savefig(path + "comparison.png")
     ### With Double Buffering
     db_df = read_nvprof_data(path + "double_buffering.csv")
     db_st_df = read_nvprof_data(path + "double_buffering_swizzled_threads.csv")
@@ -90,42 +93,42 @@ if args.test == 2:
     cutlass_df = read_nvprof_data(base_path + "cutlass.csv")
     cublas_df = read_nvprof_data(base_path + "cublas.csv")
     combined_df_db = pd.concat([db_df, db_st_df, db_stb_df, db_st_stb_df, db_v_st_df, db_v_st_stb_df, cutlass_df, cublas_df], axis=1)
-    combined_df_db.columns = ["db", "db_v", "db_st", "db_v_st", "cutlass", "cublas"]
+    combined_df_db.columns = ["db", "db_v", "db_st", "db_v_st", ".", ",", "cutlass", "cublas"]
     fig_db = plt.figure()
-    sns.violinplot(data=combined_df_db).set(xticklabels=["DB", "DB+ST", "DB+STB", "DB+V+ST", "CUTLASS", "cuBLAS"], ylabel="Runtime [ms]", title="M = 4096, N = 4096, K = 4096 with double buffering") # , xlabel=""
-    fig_db.savefig(path + "4096_4096_4096_comparison_db.png")
+    sns.violinplot(data=combined_df_db).set(xticklabels=["DB", "DB+ST", "DB+STB", "DB+ST+STB", "DB+V+ST", "DB+V+ST+STB", "CUTLASS", "cuBLAS"], ylabel="Runtime [ms]", title="M = 4096, N = 4096, K = 4096 with double buffering") # , xlabel=""
+    fig_db.savefig(path + "comparison_db.png")
 
 ### (1024 x 8192) x (8192 x 1024)
 if args.test == 3:
-    base_path = "./performance_test_results/1024_8192_1024" + str(args.precision) + "bit/"
+    base_path = "./performance_test_results/1024_1024_8192_" + str(args.precision) + "bit/"
     path = base_path + str(args.path) +'/'
     split_k_1 = read_nvprof_data(path + "split_k_1.csv")
     split_k_2 = read_nvprof_data(path + "split_k_2.csv")
-    split_k_3 = read_nvprof_data(path + "split_k_3.csv")
+    # split_k_3 = read_nvprof_data(path + "split_k_3.csv")
     split_k_4 = read_nvprof_data(path + "split_k_4.csv")
-    split_k_5 = read_nvprof_data(path + "split_k_5.csv")
-    split_k_6 = read_nvprof_data(path + "split_k_6.csv")
-    split_k_7 = read_nvprof_data(path + "split_k_7.csv")
+    # split_k_5 = read_nvprof_data(path + "split_k_5.csv")
+    # split_k_6 = read_nvprof_data(path + "split_k_6.csv")
+    # split_k_7 = read_nvprof_data(path + "split_k_7.csv")
     split_k_8 = read_nvprof_data(path + "split_k_8.csv")
-    split_k_9 = read_nvprof_data(path + "split_k_9.csv")
-    split_k_10 = read_nvprof_data(path + "split_k_10.csv")
-    split_k_11 = read_nvprof_data(path + "split_k_11.csv")
-    split_k_12 = read_nvprof_data(path + "split_k_12.csv")
-    split_k_13 = read_nvprof_data(path + "split_k_13.csv")
-    split_k_14 = read_nvprof_data(path + "split_k_14.csv")
-    split_k_15 = read_nvprof_data(path + "split_k_15.csv")
+    # split_k_9 = read_nvprof_data(path + "split_k_9.csv")
+    # split_k_10 = read_nvprof_data(path + "split_k_10.csv")
+    # split_k_11 = read_nvprof_data(path + "split_k_11.csv")
+    # split_k_12 = read_nvprof_data(path + "split_k_12.csv")
+    # split_k_13 = read_nvprof_data(path + "split_k_13.csv")
+    # split_k_14 = read_nvprof_data(path + "split_k_14.csv")
+    # split_k_15 = read_nvprof_data(path + "split_k_15.csv")
     split_k_16 = read_nvprof_data(path + "split_k_16.csv")
 
     cublas_df = read_nvprof_data(base_path + "cublas.csv")
-    combined_df_db = pd.concat([split_k_1, split_k_2, split_k_3, split_k_4, split_k_5, split_k_6, split_k_7, split_k_8, split_k_9, split_k_10, split_k_11, split_k_12, split_k_13, split_k_14, split_k_15, split_k_16, cublas_df], axis=1)
-    combined_df_db.columns = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "cublas"]
+    combined_df_db = pd.concat([split_k_1, split_k_2, split_k_4, split_k_8, split_k_16, cublas_df], axis=1)
+    combined_df_db.columns = ["-", "2", "4", "8", "16", "cublas"]
     fig_db = plt.figure()
-    sns.violinplot(data=combined_df_db).set(xticklabels=["-", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "cuBLAS"], ylabel="Runtime [ms]", title="Split K performance: M = 1024, N = 1024, K = 8192", xlabel="Split K") # , xlabel=""
+    sns.violinplot(data=combined_df_db).set(xticklabels=["-", "2", "4", "8", "16", "cuBLAS"], ylabel="Runtime [ms]", title="Split K performance: M = 1024, N = 1024, K = 8192", xlabel="Split K") # , xlabel=""
     fig_db.savefig(path + "comparison.png")
 
 ### (256 x 10240) x (10240 x 256)
 if args.test == 4:
-    base_path = "./performance_test_results/256_10240_256" + str(args.precision) + "bit/"
+    base_path = "./performance_test_results/256_256_10240_" + str(args.precision) + "bit/"
     path = base_path + str(args.path) +'/'
     split_k_1 = read_nvprof_data(path + "split_k_1.csv")
     split_k_2 = read_nvprof_data(path + "split_k_2.csv")
