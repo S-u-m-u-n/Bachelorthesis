83f4f0bac389df1d17dfc9586bf68cc163acac68
jacobsi <jacobsi@student.ethz.ch>
2021-12-01 15:52:07 +0100

    slightly change schedule

--- Milestone_2/sdfg_api_v2.py
+++ Milestone_2/sdfg_api_v2.py
@@ -79,7 +79,7 @@ if args.precision == 32:
     ndtype = np.float32
     veclen = 4
     # schedule = Schedule(load_k=4, thread_tile_m=4, thread_tile_n=4, thread_tile_k=4, warp_tile_m=32, warp_tile_n=16, thread_block_tile_m=64, thread_block_tile_n=64)
-    schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=8, warp_tile_m=32, warp_tile_n=64, thread_block_tile_m=128, thread_block_tile_n=128)
+    schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=8, warp_tile_m=64, warp_tile_n=32, thread_block_tile_m=128, thread_block_tile_n=128)
 
 else:
     dtype = dace.float64
@@ -93,6 +93,7 @@ else:
     # schedule = Schedule(load_k=4, thread_tile_m=8, thread_tile_n=8, thread_tile_k=4, warp_tile_m=64, warp_tile_n=32, thread_block_tile_m=128, thread_block_tile_n=64)
     # schedule = Schedule(load_k=2, thread_tile_m=8, thread_tile_n=8, thread_tile_k=2, warp_tile_m=64, warp_tile_n=32, thread_block_tile_m=128, thread_block_tile_n=64)
     # schedule = Schedule(load_k=1, thread_tile_m=8, thread_tile_n=8, thread_tile_k=1, warp_tile_m=64, warp_tile_n=32, thread_block_tile_m=128, thread_block_tile_n=64)
+    # schedule = Schedule(load_k=1, thread_tile_m=1, thread_tile_n=1, thread_tile_k=1, warp_tile_m=8, warp_tile_n=4, thread_block_tile_m=16, thread_block_tile_n=8)
 
 if args.split_k > 1:
     if not args.split_k_3d and not args.split_k_seq:
@@ -101,17 +102,9 @@ if args.split_k > 1:
     if args.split_k_3d and args.split_k_seq:
         raise ValueError('Only one of --split-k-3d or --split-k-seq can be used!')
 
-
-# schedule = Schedule(load_k=1, thread_tile_m=1, thread_tile_n=1, thread_tile_k=1, warp_tile_m=8, warp_tile_n=4, thread_block_tile_m=16, thread_block_tile_n=8)
-
 M = dace.symbol('M')
 N = dace.symbol('N')
 K = dace.symbol('K')
-# M_example = args.M
-# N_example = args.N
-# K_example = args.K
-# alpha = args.alpha
-# beta = args.beta
 
 sdfg = dace.SDFG('gemm')
 state = sdfg.add_state(label='gemm_state')
@@ -444,8 +437,8 @@ else:
     bitwise_and = sy.Function('bitwise_and')
     bitwise_or = sy.Function('bitwise_or')
     right_shift = sy.Function('right_shift')
-    thread_j_idx = '(thread_i // size_warp_tile_m)*size_warp_tile_m + size_thread_tile_m * bitwise_and(right_shift(warp_width * ((thread_i % size_warp_tile_m) / size_thread_tile_m) + ((thread_j % size_warp_tile_n) / size_thread_tile_n), 1), warp_height - 1)'
-    thread_i_idx = '(thread_j // size_warp_tile_n)*size_warp_tile_n + size_thread_tile_n * bitwise_or(right_shift(bitwise_and(warp_width * ((thread_i % size_warp_tile_m) / size_thread_tile_m) + ((thread_j % size_warp_tile_n) / size_thread_tile_n), warp_height * warp_width // 2), warp_width - 1), bitwise_and(warp_width * ((thread_i % size_warp_tile_m) / size_thread_tile_m) + ((thread_j % size_warp_tile_n) / size_thread_tile_n), 1))'
+    thread_i_idx = '(thread_i // size_warp_tile_m)*size_warp_tile_m + size_thread_tile_m * bitwise_and(right_shift(warp_width * ((thread_i % size_warp_tile_m) / size_thread_tile_m) + ((thread_j % size_warp_tile_n) / size_thread_tile_n), 1), warp_height - 1)'
+    thread_j_idx = '(thread_j // size_warp_tile_n)*size_warp_tile_n + size_thread_tile_n * bitwise_or(right_shift(bitwise_and(warp_width * ((thread_i % size_warp_tile_m) / size_thread_tile_m) + ((thread_j % size_warp_tile_n) / size_thread_tile_n), warp_height * warp_width // 2), warp_width - 1), bitwise_and(warp_width * ((thread_i % size_warp_tile_m) / size_thread_tile_m) + ((thread_j % size_warp_tile_n) / size_thread_tile_n), 1))'
 
 ####################################################################################################################
 ### Data Movement: _A
