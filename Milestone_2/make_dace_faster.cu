070c6d4d7964d377db1555bf7ea4c0152fedf295
jacobsi <jacobsi@student.ethz.ch>
2022-01-21 14:08:37 +0100

    try cosmaSgemm kernel

--- Milestone_2/make_dace_faster.cu
+++ Milestone_2/make_dace_faster.cu
@@ -3616,9 +3616,9 @@ void __dace_runkernel_Thread_block_grid_1_1_3(gemm_t *__state, const float * __r
     void  *Thread_block_grid_1_1_3_args[] = { (void *)&input_A, (void *)&input_B, (void *)&output, (void *)&K, (void *)&M, (void *)&N };
 
     // can use this line as a correctness check
-    cudaLaunchKernel((void*)Thread_block_grid_1_1_3, dim3(int_ceil(num_thread_blocks_n, 1), int_ceil(num_thread_blocks_m, 1), 1), dim3(max(1, num_threads_per_threadblock), 1, 1), Thread_block_grid_1_1_3_args, 0, __state->gpu_context->streams[0]);
+    // cudaLaunchKernel((void*)Thread_block_grid_1_1_3, dim3(int_ceil(num_thread_blocks_n, 1), int_ceil(num_thread_blocks_m, 1), 1), dim3(max(1, num_threads_per_threadblock), 1, 1), Thread_block_grid_1_1_3_args, 0, __state->gpu_context->streams[0]);
 
-    /*
+    // /*
     // Warmup
     for (int i = 0; i < 10; ++i) {
         cudaLaunchKernel((void*)Thread_block_grid_1_1_3, dim3(int_ceil(num_thread_blocks_n, 1), int_ceil(num_thread_blocks_m, 1), 1), dim3(max(1, num_threads_per_threadblock), 1, 1), Thread_block_grid_1_1_3_args, 0, __state->gpu_context->streams[0]);
@@ -3634,5 +3634,5 @@ void __dace_runkernel_Thread_block_grid_1_1_3(gemm_t *__state, const float * __r
 	}
 	cudaProfilerStop();
 	std::cout << "\n";
-    */
+    // */
 }
