c9e5609b330d518df1f7bcf15e3e03ff3bdfc16a
jacobsi <jacobsi@student.ethz.ch>
2022-01-21 14:14:02 +0100

    try cosmaSgemm kernel

--- Milestone_2/make_dace_faster.cu
+++ Milestone_2/make_dace_faster.cu
@@ -3620,7 +3620,7 @@ void __dace_runkernel_Thread_block_grid_1_1_3(gemm_t *__state, const float * __r
 
     // /*
     // Warmup
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < 1000; ++i) {
         cudaLaunchKernel((void*)Thread_block_grid_1_1_3, dim3(int_ceil(num_thread_blocks_n, 1), int_ceil(num_thread_blocks_m, 1), 1), dim3(max(1, num_threads_per_threadblock), 1, 1), Thread_block_grid_1_1_3_args, 0, __state->gpu_context->streams[0]);
 		std::cout << "." << std::flush; // Use dots to measure progress
 	}
