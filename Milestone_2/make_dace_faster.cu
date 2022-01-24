6a9d3e7134e5ba9c0b7421d63e022eec72020f66
jacobsi <jacobsi@student.ethz.ch>
2022-01-24 14:16:23 +0100

    try cosmaSgemm kernel

--- Milestone_2/make_dace_faster.cu
+++ Milestone_2/make_dace_faster.cu
@@ -3171,8 +3171,8 @@ DACE_DFI void nested_nested_state_1_1_5(const float * input_A, const float * inp
 
     int thread = threadIdx.x;
     if(thread == 0) {
-        printf("input_A[0] = %d", input_A[0])
-        printf("input_B[0] = %d", input_B[0])
+        printf("input_A[0] = %d", input_A[0]);
+        printf("input_B[0] = %d", input_B[0]);
     }
     const int WarpId = threadIdx.x / 32;
     const int threadId = threadIdx.x % 32;
