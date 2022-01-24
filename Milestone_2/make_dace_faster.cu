ed121b18613a8eebb7e983f049b516f79d519211
jacobsi <jacobsi@student.ethz.ch>
2022-01-24 14:28:14 +0100

    try cosmaSgemm kernel

--- Milestone_2/make_dace_faster.cu
+++ Milestone_2/make_dace_faster.cu
@@ -3245,8 +3245,8 @@ DACE_DFI void nested_nested_state_1_1_5(const float * input_A, const float * inp
     }
 
     if(thread == 0 && block_idx_x == 0 && block_idx_y == 0) {
-        printf("input_A[0] = %d", &input_A[0]);
-        printf("input_B[0] = %d", &input_B[0]);
+        printf("input_A[0] = %d\n", *input_A);
+        printf("input_B[0] = %d\n", *input_B);
     }
 
     // register TYPE Thread_Tile[THREAD_TILE_M * THREAD_TILE_N];
@@ -3330,9 +3330,9 @@ DACE_DFI void nested_nested_state_1_1_5(const float * input_A, const float * inp
             }
 
             if (k == LOAD_K - 1) {
-                // load_Global<A_VECTOR_4, A_VECTOR_2, B_VECTOR_4, B_VECTOR_2, (THREADBLOCK_TILE_K * SPLIT_K - K_ > LOAD_K), false>(&A_Shared, &B_Shared, input_A, input_B, lda, ldb, cta_k, block_idx_x, block_idx_y, A_Shared_Offset_1, B_Shared_Offset_1);
-                dace::GlobalToShared2D<float, max(1, num_threads_per_threadblock), 1, 1, size_thread_block_tile_m, size_K_tile, 1, 128, true>(input_A + ((K * size_thread_block_tile_m) * block_idx_y) + cta_k, K, 1, A_Shared + A_Shared_Offset_1);
-                dace::GlobalToShared2D<float, max(1, num_threads_per_threadblock), 1, 1, size_K_tile, size_thread_block_tile_n, 128, 1, true>(input_B + (K * cta_k) + (size_thread_block_tile_n * block_idx_x), N, 1, B_Shared + B_Shared_Offset_1);
+                load_Global<A_VECTOR_4, A_VECTOR_2, B_VECTOR_4, B_VECTOR_2, (THREADBLOCK_TILE_K * SPLIT_K - K_ > LOAD_K), false>(&A_Shared, &B_Shared, input_A, input_B, lda, ldb, cta_k, block_idx_x, block_idx_y, A_Shared_Offset_1, B_Shared_Offset_1);
+                // dace::GlobalToShared2D<float, max(1, num_threads_per_threadblock), 1, 1, size_thread_block_tile_m, size_K_tile, 1, 128, true>(input_A + ((K * size_thread_block_tile_m) * block_idx_y) + cta_k, K, 1, A_Shared + A_Shared_Offset_1);
+                // dace::GlobalToShared2D<float, max(1, num_threads_per_threadblock), 1, 1, size_K_tile, size_thread_block_tile_n, 128, 1, true>(input_B + (K * cta_k) + (size_thread_block_tile_n * block_idx_x), N, 1, B_Shared + B_Shared_Offset_1);
 
                 // load_B_Global<B_VECTOR_4, B_VECTOR_2, (THREADBLOCK_TILE_K * SPLIT_K - K_ > LOAD_K), false>(&B_Shared, input_B, ldb, cta_k, block_idx_x, B_Shared_Offset_1);
 
@@ -3382,12 +3382,12 @@ DACE_DFI void nested_nested_state_1_1_5(const float * input_A, const float * inp
         }
 
         if (k % 2 == 0) {
-            if(block_idx_x == 0 && block_idx_y == 0 && thread == 0 && k == 0) {
+            if(block_idx_x == 0 && block_idx_y == 0 && threadIdx.x == 0 && k == 0) {
                 printf("A_Shared[%d] = %d\n", A_Shared_Offset_0, A_Shared[A_Shared_Offset_0]);
                 printf("A_Shared[%d] = %d\n", A_Shared_Offset_0 + 1, A_Shared[A_Shared_Offset_0 + 1]);
                 printf("A_Shared[%d] = %d\n", A_Shared_Offset_0 + 2, A_Shared[A_Shared_Offset_0 + 2]);
                 printf("A_Shared[%d] = %d\n", A_Shared_Offset_0 + 3, A_Shared[A_Shared_Offset_0 + 3]);
-                printf("A_Shared[%d] = %d\n", A_Shared_Offset_0 + 4, &A_Shared[A_Shared_Offset_0 + 4]);
+                printf("A_Shared[%d] = %d\n", A_Shared_Offset_0 + 4, *(A_Shared + (A_Shared_Offset_0 + 4)));
                 printf("A_Shared[%d] = %d\n", A_Shared_Offset_0 + 5, &A_Shared[A_Shared_Offset_0 + 5]);
                 printf("A_Shared[%d] = %d\n", A_Shared_Offset_0 + 6, &A_Shared[A_Shared_Offset_0 + 6]);
                 printf("A_Shared[%d] = %d\n", A_Shared_Offset_0 + 7, &A_Shared[A_Shared_Offset_0 + 7]);
