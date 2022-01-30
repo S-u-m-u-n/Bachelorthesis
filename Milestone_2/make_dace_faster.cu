d7c46312f294ad9c82bbb59f1218994659715419
jacobsi <jacobsi@student.ethz.ch>
2022-01-30 21:11:09 +0100

    try cosmaSgemm kernel

--- Milestone_2/make_dace_faster.cu
+++ Milestone_2/make_dace_faster.cu
@@ -3324,15 +3324,14 @@ DACE_DFI void nested_nested_state_1_1_5(const float * input_A, const float * inp
                 // load_Shared(&A_Shared, &A_register_0, &B_Shared, &B_register_0, k, WarpIdx, WarpIdy, LaneIdx, LaneIdy, A_Shared_Offset_0, B_Shared_Offset_0);
                 // load_A_Shared(&A_Shared, &A_register_0, k, WarpIdy, LaneIdy, A_Shared_Offset_0);
                 dace::CopyND<float, 1, false, size_thread_tile_m / 2>::template ConstDst<1>::Copy(
-                    A_Shared + (((k + ((8 * size_thread_tile_m) * bitwise_and(right_shift((thread % 32), 1), (warp_height - 1)))) + ((8 * size_warp_tile_m) * ((thread / 32) / num_warps_n))) + A_Shared_Offset_0), A_register_0, 128);
-
-                    // A_Shared + (((128 * k + ((8 * size_thread_tile_m / 2) * bitwise_and(right_shift((thread % 32), 1), (warp_height - 1)))) + ((8 * size_warp_tile_m) * ((thread / 32) / num_warps_n))) + A_Shared_Offset_0), A_register_0, 1);
+                    A_Shared + (((128 * k + ((size_thread_tile_m / 2) * bitwise_and(right_shift((thread % 32), 1), (warp_height - 1)))) + ((size_warp_tile_m) * ((thread / 32) / num_warps_n))) + A_Shared_Offset_0), A_register_0, 1);
                     // A_Shared + A_Shared_Offset_0 + 128 * k + WarpIdy * size_warp_tile_m + 4 * LaneIdy, A_register_0, 1);
                     
 
                 dace::CopyND<float, 1, false, size_thread_tile_m / 2>::template ConstDst<1>::Copy(
                     // A_Shared + (((128 * k + ((8 * size_thread_tile_m / 2) * bitwise_and(right_shift((thread % 32), 1), (warp_height - 1)))) + ((8 * size_warp_tile_m) * ((thread / 32) / num_warps_n))) + A_Shared_Offset_0) + 16, A_register_0 + 4, 1);
-                    A_Shared + A_Shared_Offset_0 + 128 * k + WarpIdy * size_warp_tile_m + 4 * LaneIdy + 16, A_register_0 + 4, 1);
+                    // A_Shared + A_Shared_Offset_0 + 128 * k + WarpIdy * size_warp_tile_m + 4 * LaneIdy + 16, A_register_0 + 4, 1);
+                    A_Shared + (((128 * k + ((size_thread_tile_m / 2) * bitwise_and(right_shift((thread % 32), 1), (warp_height - 1)))) + ((size_warp_tile_m) * ((thread / 32) / num_warps_n))) + A_Shared_Offset_0 + 16), A_register_0 + 4, 1);
 
                 // constexpr int M_THREADS = WARP_TILE_M / THREAD_TILE_M; // = 4
 
