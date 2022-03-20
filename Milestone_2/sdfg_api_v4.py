0fe0b78c34fbaf20856d080238c6057f1e1a7a75
jacobsi <jacobsi@student.ethz.ch>
2022-03-20 20:22:20 +0100

    try things

--- Milestone_2/sdfg_api_v4.py
+++ Milestone_2/sdfg_api_v4.py
@@ -102,20 +102,25 @@ if args.precision == 32:
     args.beta = ndtype(args.beta)
     # for split k scenarios, this schedule seems to be the best:
     schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=2, warp_tile_m=16, warp_tile_n=32, thread_block_tile_m=32, thread_block_tile_n=128)
+
+    # More similar to cucosma:
+    # schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=8, warp_tile_m=32, warp_tile_n=64, thread_block_tile_m=128, thread_block_tile_n=128)
+
     # schedule = Schedule(load_k=4, thread_tile_m=4, thread_tile_n=4, warp_tile_m=32, warp_tile_n=16, thread_block_tile_m=64, thread_block_tile_n=64)
     # schedule = Schedule(load_k=4, thread_tile_m=8, thread_tile_n=8, warp_tile_m=32, warp_tile_n=64, thread_block_tile_m=128, thread_block_tile_n=128)
 
     # schedule = Schedule(load_k=4, thread_tile_m=4, thread_tile_n=4, warp_tile_m=16, warp_tile_n=32, thread_block_tile_m=32, thread_block_tile_n=128)
     # schedule = Schedule(load_k=4, thread_tile_m=4, thread_tile_n=4, warp_tile_m=16, warp_tile_n=32, thread_block_tile_m=32, thread_block_tile_n=64)
     # schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=8, warp_tile_m=64, warp_tile_n=32, thread_block_tile_m=128, thread_block_tile_n=128)
-    # schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=8, warp_tile_m=32, warp_tile_n=64, thread_block_tile_m=128, thread_block_tile_n=128)
 
 else:
     dtype = dace.float64
     ndtype = np.float64
     veclen = 2
     # schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=8, warp_tile_m=64, warp_tile_n=32, thread_block_tile_m=128, thread_block_tile_n=64)
-    schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=8, warp_tile_m=32, warp_tile_n=64, thread_block_tile_m=128, thread_block_tile_n=64)
+    schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=2, warp_tile_m=16, warp_tile_n=32, thread_block_tile_m=32, thread_block_tile_n=128)
+
+    # schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=8, warp_tile_m=32, warp_tile_n=64, thread_block_tile_m=128, thread_block_tile_n=64)
     # schedule = Schedule(load_k=16, thread_tile_m=4, thread_tile_n=4, warp_tile_m=32, warp_tile_n=16, thread_block_tile_m=64, thread_block_tile_n=32)
     # schedule = Schedule(load_k=4, thread_tile_m=4, thread_tile_n=4, warp_tile_m=32, warp_tile_n=16, thread_block_tile_m=128, thread_block_tile_n=64)
     # schedule = Schedule(load_k=8, thread_tile_m=2, thread_tile_n=2, warp_tile_m=16, warp_tile_n=8, thread_block_tile_m=128, thread_block_tile_n=64)
