6dc556760c46ecb35c3ae1dd40bbf365c65de770
jacobsi <jacobsi@student.ethz.ch>
2022-03-20 18:47:06 +0100

    try things

--- Milestone_2/sdfg_api_v4.py
+++ Milestone_2/sdfg_api_v4.py
@@ -101,7 +101,7 @@ if args.precision == 32:
     args.alpha = ndtype(args.alpha)
     args.beta = ndtype(args.beta)
     # schedule = Schedule(load_k=4, thread_tile_m=4, thread_tile_n=4, warp_tile_m=32, warp_tile_n=16, thread_block_tile_m=64, thread_block_tile_n=64)
-    schedule = Schedule(load_k=4, thread_tile_m=4, thread_tile_n=4, warp_tile_m=32, warp_tile_n=16, thread_block_tile_m=32, thread_block_tile_n=128)
+    schedule = Schedule(load_k=4, thread_tile_m=4, thread_tile_n=4, warp_tile_m=16, warp_tile_n=32, thread_block_tile_m=32, thread_block_tile_n=128)
     # schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=8, warp_tile_m=64, warp_tile_n=32, thread_block_tile_m=128, thread_block_tile_n=128)
     # schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=8, warp_tile_m=32, warp_tile_n=64, thread_block_tile_m=128, thread_block_tile_n=128)
 
