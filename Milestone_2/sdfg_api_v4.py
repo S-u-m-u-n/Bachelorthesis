ddd07b8accfa54cba052392620d282ab4aa4013b
jacobsi <jacobsi@student.ethz.ch>
2022-03-21 19:05:37 +0100

    try things

--- Milestone_2/sdfg_api_v4.py
+++ Milestone_2/sdfg_api_v4.py
@@ -116,7 +116,8 @@ if args.precision == 32:
         # schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=8, warp_tile_m=32, warp_tile_n=64, thread_block_tile_m=128, thread_block_tile_n=128) # cuCOSMA's schedule
         # schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=4, warp_tile_m=16, warp_tile_n=32, thread_block_tile_m=64, thread_block_tile_n=64) # Only uses 16 threads/warp, still fast... wtf?
         # schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=4, warp_tile_m=16, warp_tile_n=16, thread_block_tile_m=32, thread_block_tile_n=128) # Fastest, wrong
-        schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=4, warp_tile_m=32, warp_tile_n=32, thread_block_tile_m=32, thread_block_tile_n=128) # 
+        # schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=4, warp_tile_m=32, warp_tile_n=32, thread_block_tile_m=32, thread_block_tile_n=128) # 1.8ms, correct
+        schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=8, warp_tile_m=64, warp_tile_n=32, thread_block_tile_m=64, thread_block_tile_n=128) # 1.8ms
     elif args.M == 256 and args.N == 256 and args.K == 10240:
         schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=2, warp_tile_m=16, warp_tile_n=32, thread_block_tile_m=32, thread_block_tile_n=128)
     else:
