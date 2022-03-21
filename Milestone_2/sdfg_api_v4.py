6f431302d120e83ea3603001c02908f999574962
jacobsi <jacobsi@student.ethz.ch>
2022-03-21 02:18:28 +0100

    try things

--- Milestone_2/sdfg_api_v4.py
+++ Milestone_2/sdfg_api_v4.py
@@ -106,7 +106,7 @@ if args.precision == 32:
     elif args.M == 4096 and args.N == 4096 and args.K == 4096:
         schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=2, warp_tile_m=16, warp_tile_n=32, thread_block_tile_m=32, thread_block_tile_n=128)
     elif args.M == 1024 and args.N == 1024 and args.K == 8192:
-        schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=16, warp_tile_m=64, warp_tile_n=64, thread_block_tile_m=64, thread_block_tile_n=64)
+        schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=16, warp_tile_m=64, warp_tile_n=64, thread_block_tile_m=128, thread_block_tile_n=128)
     elif args.M == 256 and args.N == 256 and args.K == 10240:
         schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=2, warp_tile_m=16, warp_tile_n=32, thread_block_tile_m=32, thread_block_tile_n=128)
     else:
