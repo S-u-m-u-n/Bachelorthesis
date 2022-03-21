abbe5e6fe3f2b00fdb3e9cc32fcb6d889bb0edde
jacobsi <jacobsi@student.ethz.ch>
2022-03-21 02:07:03 +0100

    try things

--- Milestone_2/sdfg_api_v4.py
+++ Milestone_2/sdfg_api_v4.py
@@ -108,6 +108,7 @@ if args.precision == 32:
     elif args.M == 1024 and args.N == 1024 and args.K == 8192:
         schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=2, warp_tile_m=16, warp_tile_n=32, thread_block_tile_m=32, thread_block_tile_n=128)
     elif args.M == 256 and args.N == 256 and args.K == 10240:
+        print("Using specific schedule...")
         schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=2, warp_tile_m=16, warp_tile_n=32, thread_block_tile_m=32, thread_block_tile_n=128)
     else:
         schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=8, warp_tile_m=32, warp_tile_n=64, thread_block_tile_m=128, thread_block_tile_n=128)
