7cd47eebafe95544fc75e7059437a7994a8308d0
jacobsi <jacobsi@student.ethz.ch>
2022-03-21 02:38:38 +0100

    try things

--- Milestone_2/sdfg_api_v4.py
+++ Milestone_2/sdfg_api_v4.py
@@ -102,8 +102,8 @@ if args.precision == 32:
     args.beta = ndtype(args.beta)
 
     if args.M == 1024 and args.N == 1024 and args.K == 1024:
-        schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=8, warp_tile_m=32, warp_tile_n=64, thread_block_tile_m=128, thread_block_tile_n=128) # cuCOSMA's schedule
-        # schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=2, warp_tile_m=16, warp_tile_n=32, thread_block_tile_m=32, thread_block_tile_n=128)
+        # schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=8, warp_tile_m=32, warp_tile_n=64, thread_block_tile_m=128, thread_block_tile_n=128) # cuCOSMA's schedule
+        schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=4, warp_tile_m=16, warp_tile_n=32, thread_block_tile_m=32, thread_block_tile_n=128)
     elif args.M == 4096 and args.N == 4096 and args.K == 4096:
         schedule = Schedule(load_k=8, thread_tile_m=8, thread_tile_n=2, warp_tile_m=16, warp_tile_n=32, thread_block_tile_m=32, thread_block_tile_n=128)
     elif args.M == 1024 and args.N == 1024 and args.K == 8192:
